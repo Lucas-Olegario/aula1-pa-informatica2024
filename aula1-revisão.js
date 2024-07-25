81c73580a3773b1373979b662b40e7860fa112aa
Lucas Olegário Alves da Silva <pekosolucas@gmail.com>
2024-07-25 12:17:49 -0300

    Aula1-Pa-3-Bimestre
    meu conteudo da aula 1 do primeiro Bimestre

---
+++ aula1-revisão.js
@@ -0,0 +1,16 @@
+// Criando as variaveis:
+let saudacao= "Olá Mundo"; // Tipo de dado "String"
+let numero= 25; //Tipo de dado Inteiro(int)
+let decimal= 3.14; // Tipo de dado Flutuante (float)
+let verdadeiro= true; // Tipo de dado booleano ( verdadeiro)
+let falso= false; // Tipo de dado booleano (falso)
+
+//Saida dos dados atribuidos nas variáveis:
+console.log(saudacao);
+console.log("Saudação: ", saudacao);
+console.log(" ");
+console.log("numero: ", numero);
+console.log("decimal: ", decimal);
+console.log("verdadeiro: ", verdadeiro);
+console.log("falso: ", falso);
+console.log(" ");
